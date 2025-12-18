9fc9b12c10d2e368f8718057a0fa0754eb120cdc
Antonio Queb <cardstillo@gmail.com>
2025-12-17 21:18:28 -0600

    Actualizar versión del módulo a 19.0.1.0.1 en el manifiesto.

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # ./__manifest__.py
 {
     'name': 'Carrito de Compra para Inventario Visual',
-    'version': '19.0.1.0.0',
+    'version': '19.0.1.0.1',
     'category': 'Inventory/Inventory',
     'summary': 'Sistema de carrito de compra y apartado múltiple desde inventario visual',
     'author': 'Alphaqueb Consulting SAS',
