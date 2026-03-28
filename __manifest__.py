0baa692ed78673002cac07c7ea451703eabd9b57
Antonio Queb <cardstillo@gmail.com>
2026-03-28 15:55:59 -0600

    fix: actualizar versión del módulo y renombrar menú de configuración de precios a utilidades

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # ./__manifest__.py
 {
     'name': 'Carrito de Compra para Inventario Visual',
-    'version': '19.0.1.0.1',
+    'version': '19.0.2.0.1',
     'category': 'Inventory/Inventory',
     'summary': 'Sistema de carrito de compra y apartado múltiple desde inventario visual',
     'author': 'Alphaqueb Consulting SAS',
